78a4dd34cbe17b56abb03cf0d179b1abf132fb69
Anna K <tanarri.nabassu@gmail.com>
2019-11-01 11:00:55 +0300

    Add function for Pirson and Spearman cor tests

---
+++ ps.R
@@ -0,0 +1,14 @@
+# df - data frame (two columns with numbers)
+# method - pirson or spearman
+
+pirspear <- function(df, method = 'pirson'){
+  if (method == 'pirson'){
+    return ((sum((df[,2]-mean(df[,2]))*(df[,1]-mean(df[,1])))/(length(df[,1])-1))/(sd(df[,1])*sd(df[,2])))
+  } else if (method == 'spearman') {
+    df <- df[order(df[,1]),]
+    df[,1] <- c(1:length(df[,1]))
+    df <- df[order(df[,2]),]
+    df[,2] <- c(1:length(df[,2]))
+    return (1-((6*(sum((df[,1]-df[,2])^2)))/(length(df[,1])*((length(df[,1])^2)-1))))
+  }
+}
